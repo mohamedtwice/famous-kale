f3e6baec81fe5a4ec3677e8b92cc899b0e0461f1
Stackbit <projects@stackbit.com>
2019-09-18 16:02:58 +0000

    initial commit by Stackbit

---
+++ stackbit-build.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+set -v
+
+initialGitHash=$(git rev-list --max-parents=0 HEAD)
+node ./studio-build.js $initialGitHash &
+
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d825429e9453a0013bf780c
+gatsby build
+
+wait
+echo "Stackbit-build.sh finished build"
